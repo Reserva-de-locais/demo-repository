b0115b7373aa8a560c2582fd833f3a03b3e57fa7
kaueRodrgues <r.kaue@escolar.ifrn.edu.br>
2026-07-10 08:16:32 -0300

    Update security.py

--- security.py
+++ security.py
@@ -1,12 +1,8 @@
-import os
 import hashlib
-
 from fastapi import Header, HTTPException
-from dotenv import load_dotenv
 
-load_dotenv()
 
-API_KEY_HASH = os.getenv("API_KEY_HASH")
+API_KEY_HASH = aa9c02821247579e02f7c6af1236820c9d8a3f2ebf852685f5cddaf760534257
 
 
 def verificar_api_key(x_api_key: str = Header(...)):
@@ -16,7 +12,6 @@ def verificar_api_key(x_api_key: str = Header(...)):
     ).hexdigest()
 
     if hash_recebido != API_KEY_HASH:
-
         raise HTTPException(
             status_code=401,
             detail="API Key inválida."
