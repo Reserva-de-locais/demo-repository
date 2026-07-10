c1fb5089b24f769cff62d8264aa8d424beade719
kaueRodrgues <r.kaue@escolar.ifrn.edu.br>
2026-07-10 07:46:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -102,17 +102,16 @@ def login(dados: LoginEntrada):
             (dados.email, dados.senha)
         ).fetchone()
 
-    if usuario is None:
-        raise HTTPException(
-            status_code=401,
-            detail="Email ou senha inválidos."
-        )
+if usuario is None:
+    raise HTTPException(
+        status_code=401,
+        detail="Email ou senha inválidos."
+    )
 
-  return {
+return {
     "mensagem": "Login realizado com sucesso!",
     "usuario": dict(usuario)
 }
-
 # ═══════════════════ ESPAÇOS ═══════════════════
 
 @app.post("/espacos", response_model=Espaco, status_code=201)
